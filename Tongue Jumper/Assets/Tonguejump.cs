5514fd80ad29c5752cb9caed0356b0ecfeae3e62
XO-Doctor <44132391+XO-Doctor@users.noreply.github.com>
2018-12-08 16:07:52 +1030

    added player

--- Tongue Jumper/Assets/Tonguejump.cs
+++ Tongue Jumper/Assets/Tonguejump.cs
@@ -9,9 +9,13 @@ public class Tonguejump : MonoBehaviour {
     public Vector3 playerpos;
     public FixedJoint2D joint;
     public Collider2D col;
-
-	// Use this for initialization
-	void Start () {
+    public float distance;
+    public GameObject tongue;
+    public float distancefromy;
+    public float distancefromx;
+    public Camera cam;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -22,7 +26,16 @@ public class Tonguejump : MonoBehaviour {
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10000 * Time.deltaTime);
         playerpos = player.transform.position;
-
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        tongue.transform.localScale = new Vector3 (distance * 5, 1, 1);
+        distancefromx = (transform.position.x + player.transform.position.x);
+        distancefromy = (transform.position.y + player.transform.position.y);
+        tongue.transform.position = new Vector3(distancefromx /2, distancefromy / 2, 0);
+        Vector2 directiont = tongue.transform.position - transform.position;
+        float anglet = Mathf.Atan2(directiont.y, directiont.x) * Mathf.Rad2Deg;
+        Quaternion rotationt = Quaternion.AngleAxis(anglet, Vector3.forward);
+        tongue.transform.rotation = Quaternion.Slerp(transform.rotation, rotationt, 10000 * Time.deltaTime);
+        cam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -30);
         if (Input.GetMouseButtonDown(0))
         {
             holding = true;
