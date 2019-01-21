336a99f93cb87a452d8d2b3d5bbbbfe85c1c04c6
XO-Doctor <44132391+XO-Doctor@users.noreply.github.com>
2019-01-22 08:27:28 +1030

    cloudsssss   dfidw8ufiqw

---
+++ Space warehouse/Assets/cloud.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour {
+
+    public Sprite lg1;
+    public Sprite lg2;
+    public Sprite ls;
+    public Sprite l;
+    private bool follow;
+    public GameObject player;
+    public SpriteRenderer sr;
+    public GameObject lightning;
+
+    // Use this for initialization
+    void Start () {
+        follow = true;
+        StartCoroutine(Wait());
+	}
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(Random.Range(5, 10));
+        follow = false;
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg2;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = l;
+        yield return new WaitForSeconds(1.5f);
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg2;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = l;
+        yield return new WaitForSeconds(1.5f);
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg2;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = lg1;
+        yield return new WaitForSeconds(0.09f);
+        sr.sprite = l;
+        yield return new WaitForSeconds(2f);
+        sr.sprite = ls;
+        Vector3 offset = new Vector3(player.transform.position.x, 4, 0);
+        Instantiate(lightning, offset, Quaternion.identity);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (follow)
+        {
+            Vector3 offset = new Vector3(player.transform.position.x, 4, 0);
+            transform.position = offset;
+        }
+	}
+}
